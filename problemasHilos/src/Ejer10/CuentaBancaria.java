b127c4162fc0b9f21581486825f8a62e56c2f9c7
raupersan <raulps1798@gmail.com>
2024-10-31 13:34:43 +0100

    Ejercicio0 practica 3 hecho

--- problemasHilos/src/Ejer10/CuentaBancaria.java
+++ problemasHilos/src/Ejer10/CuentaBancaria.java
@@ -1,69 +1,91 @@
 package Ejer10;
 
-
 class Transferencia implements Runnable {
-    private CuentaBancaria origen;
-    private CuentaBancaria destino;
-    private int cantidad;
+	private CuentaBancaria origen;
+	private CuentaBancaria destino;
+	private int cantidad;
 
-    public Transferencia(CuentaBancaria origen, CuentaBancaria destino, int cantidad) {
-        this.origen = origen;
-       this.destino = destino;
-        this.cantidad = cantidad;
-    }
+	public Transferencia(CuentaBancaria origen, CuentaBancaria destino, int cantidad) {
+		this.origen = origen;
+		this.destino = destino;
+		this.cantidad = cantidad;
+	}
 
-    @Override
-    public void run() {
-        for (int i = 0; i < 100; i++) {
-            origen.transferir(destino, cantidad);
-        }
-    }
+	@Override
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			origen.transferir(destino, cantidad);
+		}
+	}
 }
-
 
 public class CuentaBancaria {
-    private int balance;
+	private int balance;
 
-    public CuentaBancaria(int balanceInicial) {
-        this.balance = balanceInicial;
-    }
+	public CuentaBancaria(int balanceInicial) {
+		this.balance = balanceInicial;
+	}
 
-    public void depositar(int cantidad) {
-        balance += cantidad;
-    }
+	public void depositar(int cantidad) {
+		balance += cantidad;
+	}
 
-    public void retirar(int cantidad) {
-        balance -= cantidad;
-    }
+	public void retirar(int cantidad) {
+		balance -= cantidad;
+	}
 
-    public void transferir(CuentaBancaria destino, int cantidad) {
-        this.retirar(cantidad);
-        destino.depositar(cantidad);
-    }
+	public void transferir(CuentaBancaria destino, int cantidad) {
+		this.hashCode();
+		Object primero, segundo;
 
-    public int getBalance() {
-        return balance;
-    }
-    
-    public static void main(String[] args) {
-        CuentaBancaria cuenta1 = new CuentaBancaria(1000);
-        CuentaBancaria cuenta2 = new CuentaBancaria(1000);
+		// primero = cuentaBancariaMenor;
+		// segundo = cuentaBancariaMayor;
+		/* Esto puede dar un error ya que el metodo hashCode puede ser sobreescrito
+		 * if (this.hashCode() < destino.hashCode()) {
+			primero = this;
+			segundo = destino;
+			this.retirar(cantidad);
+			destino.depositar(cantidad);
+		}
+		*/
+		if(System.identityHashCode(this)<System.identityHashCode(destino)) {
+			primero=this;
+			segundo=destino;
+		}
+		else {
+			primero=destino;
+			segundo=destino;
+		}
+		synchronized(this) {
+			synchronized(segundo) {
+				this.retirar(cantidad);
+				destino.depositar(cantidad);
+			}
+		}
+	}
 
-        Thread hilo1 = new Thread(new Transferencia(cuenta1, cuenta2, 12));
-        Thread hilo2 = new Thread(new Transferencia(cuenta2, cuenta1, 10));
+	public int getBalance() {
+		return balance;
+	}
 
-        hilo1.start();
-        hilo2.start();
-        
-        try{
-        	hilo1.join();
-        	hilo2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+	public static void main(String[] args) {
+		CuentaBancaria cuenta1 = new CuentaBancaria(1000);
+		CuentaBancaria cuenta2 = new CuentaBancaria(1000);
 
-        System.out.println("Balance final de cuenta1: " + cuenta1.getBalance());
-        System.out.println("Balance final de cuenta2: " + cuenta2.getBalance());
-    }
+		Thread hilo1 = new Thread(new Transferencia(cuenta1, cuenta2, 12));
+		Thread hilo2 = new Thread(new Transferencia(cuenta2, cuenta1, 10));
+
+		hilo1.start();
+		hilo2.start();
+
+		try {
+			hilo1.join();
+			hilo2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Balance final de cuenta1: " + cuenta1.getBalance());
+		System.out.println("Balance final de cuenta2: " + cuenta2.getBalance());
+	}
 }
-
